b02212989e1d3342a072ea77bbef2a736c41691d
236490@student.pwr.edu.pl <236490@student.pwr.edu.pl>
2021-05-23 23:18:48 +0200

    next

--- cpp/Project1/main.cpp
+++ cpp/Project1/main.cpp
@@ -7,7 +7,7 @@ int main()
 	std::vector<double> qs = {0.00,0.05, 0.1, 0.02 ,0.3};
 	//simulationAverages(10000, 6, 100, 200, 0,1.0, 40, qs,"Results\\Triangular\\triangular");
 	//simulationAverages(10000, 4, 80, 100, 0,0.5, 20, qs,"Results\\Square\\square");
-	simulationAveragesNonInfty(25, 25, 100, 100, 0.005,0.6, 300, qs,"triangular", "Results\\Non_infty\\Triangular\\triangular");
-	simulationAveragesNonInfty(25, 25, 100, 100, 0.005,0.6, 300, qs,"square","Results\\Non_infty\\Square\\square");
+	simulationAveragesNonInfty(25, 25, 800, 500, 0.005,0.35, 30, qs,"triangular", "Results\\Non_infty\\Triangular\\triangular");
+	simulationAveragesNonInfty(25, 25, 800, 500, 0.005,0.35, 30, qs,"square","Results\\Non_infty\\Square\\square");
 	return 0;
 }
